b199283ab77dc89614cb639c40d9db8fd7b16b7c
omkar2809 <zemseom10@gmail.com>
2021-01-23 16:49:14 +0530

    Basic Authentication

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth')
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
